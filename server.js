e26bac7d5a96a11b0c02bb33d18def55382df2c2
oscaradrilira <oscaradrianliraflores@gmail.com>
2025-04-15 16:46:18 -0600

    export de app

--- server.js
+++ server.js
@@ -36,4 +36,4 @@ app.use("/api/vehiculos", require("./routes/VehiculosRoutes.js"));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
 
-module.exports = router;
+module.exports = app;
